7c024b5c38a0388ad02548cc6abf29639836a14c
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-11-20 16:34:24 +0100

    update: updated property slidesPerView

--- js/script-swiper.js
+++ js/script-swiper.js
@@ -11,11 +11,11 @@ Object.assign(swiperEl, {
             spaceBetween: 20,
         },
         768: {
-            slidesPerView: 4,
+            slidesPerView: 2,
             spaceBetween: 40,
         },
         1024: {
-            slidesPerView: 5,
+            slidesPerView: 4,
             spaceBetween: 50,
         },
     },
