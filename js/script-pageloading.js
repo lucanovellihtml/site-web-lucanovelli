82eb59cae094b51c1bc2081ca032ad46b44a3ea1
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-11-21 09:13:33 +0100

    add: added script to manage animation loading

---
+++ js/script-pageloading.js
@@ -0,0 +1,7 @@
+// Nascondi il loading screen e mostra il contenuto dopo un breve ritardo (simulando il caricamento)
+setTimeout(() => {
+    document.getElementById("pageLoading").style.display = "none";
+    document.getElementById("header").style.display = "block";
+    document.getElementById("main").style.display = "block";
+    document.getElementById("footer").style.display = "block";
+}, 4000);
