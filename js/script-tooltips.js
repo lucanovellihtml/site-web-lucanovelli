bde1d2b6467d6ba19ccc2e6daef1158775a9f669
LucaNovelliBoolean <160405407+lucanovellihtml@users.noreply.github.com>
2024-11-21 21:30:38 +0100

    add: added script to start a tooltip

---
+++ js/script-tooltips.js
@@ -0,0 +1,11 @@
+// tooltip per DXC Technology
+const tooltipTriggerList = document.getElementById("boxComuneDiMilano");
+const tooltipList = [tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// tooltip per EniPlenitude
+const tooltipTriggerListEni = document.getElementById("boxEni");
+const tooltipListEni = [tooltipTriggerListEni].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+
+// tooltip per EniPlenitude
+const tooltipTriggerListInterroll = document.getElementById("boxinterroll");
+const tooltipListInterroll = [tooltipTriggerListInterroll].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
